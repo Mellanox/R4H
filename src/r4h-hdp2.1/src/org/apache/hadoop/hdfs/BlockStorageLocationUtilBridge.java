07b56d07ac68f1b64b38ca6364a146636e60a6bc
Yuriy Arbitman <yuriya@mellanox.com>
2014-06-30 16:30:30 +0300

    Porting of client side from CDH4.4 to HDP2.1. This is version 0 - includes all the original client code minus non-write flows (which are irrelevant), but in r4h package. Next commit will include our RDMA code.
    Change-Id: I12478b11753585cbda07ce410f5db5c04d9c7d20

---
+++ src/r4h-hdp2.1/src/org/apache/hadoop/hdfs/BlockStorageLocationUtilBridge.java
@@ -0,0 +1,34 @@
+package org.apache.hadoop.hdfs;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.BlockStorageLocation;
+import org.apache.hadoop.fs.VolumeId;
+import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
+import org.apache.hadoop.hdfs.protocol.HdfsBlocksMetadata;
+import org.apache.hadoop.hdfs.protocol.LocatedBlock;
+import org.apache.hadoop.hdfs.security.token.block.InvalidBlockTokenException;
+
+public class BlockStorageLocationUtilBridge extends BlockStorageLocationUtil {
+
+	public static Map<DatanodeInfo, HdfsBlocksMetadata> queryDatanodesForHdfsBlocksMetadata(Configuration conf,
+	        Map<DatanodeInfo, List<LocatedBlock>> datanodeBlocks, int poolsize, int timeoutMs, boolean connectToDnViaHostname)
+	        throws InvalidBlockTokenException {
+		return BlockStorageLocationUtil.queryDatanodesForHdfsBlocksMetadata(conf, datanodeBlocks, poolsize, timeoutMs, connectToDnViaHostname);
+	}
+
+	public static Map<LocatedBlock, List<VolumeId>> associateVolumeIdsWithBlocks(List<LocatedBlock> blocks,
+	        Map<DatanodeInfo, HdfsBlocksMetadata> metadatas) {
+		return BlockStorageLocationUtil.associateVolumeIdsWithBlocks(blocks, metadatas);
+	}
+
+	
+	public   static BlockStorageLocation[] convertToVolumeBlockLocations(
+		      List<LocatedBlock> blocks, 
+		      Map<LocatedBlock, List<VolumeId>> blockVolumeIds) throws IOException {
+		return BlockStorageLocationUtil.convertToVolumeBlockLocations(blocks, blockVolumeIds);
+	}
+}
---
+++ src/r4h-hdp2.1/src/org/apache/hadoop/hdfs/ClientBridge.java
@@ -0,0 +1,10 @@
+package org.apache.hadoop.hdfs;
+
+import org.apache.hadoop.security.UserGroupInformation;
+
+public class ClientBridge {
+
+	public static String getUgiShortUserName(UserGroupInformation ugi) {
+		return ugi.getShortUserName();
+	}
+}
