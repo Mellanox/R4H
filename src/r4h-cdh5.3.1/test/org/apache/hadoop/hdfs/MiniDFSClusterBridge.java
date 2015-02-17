eed2c43e4c89d763da48249d7d95094bc67a7086
Elad Itzhakian <eladi@mellanox.com>
2015-02-17 17:42:36 +0200

    Added support for CDH 5.3.1 based on Hadoop 2.5.0
    Change-Id: Ie1df359887ddcde09dfbecfd52bc02e700bab2ba

---
+++ src/r4h-cdh5.3.1/test/org/apache/hadoop/hdfs/MiniDFSClusterBridge.java
@@ -0,0 +1,38 @@
+package  org.apache.hadoop.hdfs;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.logging.Log;
+import org.apache.hadoop.hdfs.MiniDFSNNTopology.NNConf;
+
+public class MiniDFSClusterBridge {
+	
+	public static int getAppendTestUtils_FILE_SIZE() {
+		return AppendTestUtil.FILE_SIZE;
+	}
+
+	public static int getAppendTestUtil_BLOCK_SIZE() {
+		return AppendTestUtil.BLOCK_SIZE;
+	}
+
+	public static int getNNConf_ipcPort(NNConf nnConf) {
+		return nnConf.getIpcPort();
+	}
+
+	public static String getNNConf_nnId(NNConf nnConf) {
+		return nnConf.getNnId();
+	}
+
+	public static String getNNConf_cliusterId(NNConf nnConf) {
+		return nnConf.getClusterId();
+	}
+
+	public static int getNNConf_httpPort(NNConf nnConf) {
+		return nnConf.getHttpPort();
+	}
+	
+	public static Log getAppendTestUtilLOG() {
+		return AppendTestUtil.LOG;
+	}
+	
+}
